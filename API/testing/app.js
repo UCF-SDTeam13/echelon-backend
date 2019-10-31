ffea5a570dabe5d90b43de6d6586ac48b5dafc37
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2019-10-30 21:10:43 -0400

    Fix Testing Formatting with ESLint

--- API/testing/app.js
+++ API/testing/app.js
@@ -2,114 +2,132 @@ const dotenv = require('dotenv');
 // Environment Variable Import
 dotenv.config({ path: '.env' });
 
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 
 const uuid = require('uuid');
 
 // HTTP Response
 let response;
-let gamelift = new AWS.GameLift();
+const gamelift = new AWS.GameLift();
 
+async function createGameSession(gameParams) {
+  const gameSessionPromise = gamelift.createGameSession(gameParams).promise();
+  return (await gameSessionPromise).GameSession;
+}
+
+async function createPlayerSession(playerParams) {
+  const playerSessionPromise = gamelift.createPlayerSession({
+    GameSessionId: playerParams.GameSessionId,
+    PlayerId: uuid.v4(),
+  }).promise();
+  return (await playerSessionPromise).PlayerSession;
+}
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-        console.log(event);
-        switch (event.resource) {
-            // Test Game Session POST - Create Game Session
-            case '/test/game/session':
-                console.log('Getting GameLift Session');
-                let params = {
-                    MaximumPlayerSessionCount: '2', /* required */
-                    //AliasId: 'STRING_VALUE',
-                    //CreatorId: 'STRING_VALUE',
-                    FleetId: process.env.FLEETID,
-                    GameProperties: [
-                        {
-                            Key: 'test', /* required */
-                            Value: 'STRING_VALUE' /* required */
-                        },
-                        /* more items */
-                    ],
-                    //GameSessionData: 'STRING_VALUE',
-                    //GameSessionId: 'STRING_VALUE',
-                    //IdempotencyToken: 'STRING_VALUE',
-                    //Name: 'STRING_VALUE'
-                };
+  const gameParams = {
+    MaximumPlayerSessionCount: '8', /* required */
+    // AliasId: 'STRING_VALUE',
+    // CreatorId: 'STRING_VALUE',
+    FleetId: process.env.FLEETID,
+    GameProperties: [
+      {
+        Key: 'test', /* required */
+        Value: 'STRING_VALUE', /* required */
+      },
+      /* more items */
+    ],
+    // GameSessionData: 'STRING_VALUE',
+    // GameSessionId: 'STRING_VALUE',
+    // IdempotencyToken: 'STRING_VALUE',
+    // Name: 'STRING_VALUE'
+  };
 
-                let gameSessionPromise = gamelift.createGameSession(params).promise();
-                // Because AWS
-                let gamesession = (await gameSessionPromise).GameSession;
-                // Respond with HTTP 200 OK and Token
-                console.log("200 OK");
-                response = {
-                    'statusCode': 200,
-                    'body': JSON.stringify({
-                        message: 'Session Creation Successful',
-                        GameSessionId: gamesession.GameSessionId,
-                        FleetId: gamesession.FleetId,
-                        CreationTime: gamesession.CreationTime,
-                        CurrentPlayerSessionCount: gamesession.CurrentPlayerSessionCount,
-                        MaximumPlayerSessionCount: gamesession.MaximumPlayerSessionCount,
-                        Status: gamesession.Status.toString(),
-                        IpAddress: gamesession.IpAddress.toString(),
-                        Port: gamesession.Port.toString(),
-                        PlayerSessionCreationPolicy: gamesession.PlayerSessionCreationPolicy
-                    })
-                }
+  try {
+    // Log Event
+    console.log(event);
+    // Log Console
+    console.log(context);
+    let gameSession;
+    let playerSession;
+    let playerParams;
+    switch (event.resource) {
+      // Test Game Session POST - Create Game Session
+      case '/test/game/session':
+        console.log('Getting GameLift Session');
 
-                break;
-            // Test Player Session POST - Create Player Session in Game Session
-            case '/test/player/session':
-                if (event.body) {
-                    let params = JSON.parse(event.body);
-                    console.log('Creating Player Session');
-                    let playerSessionPromise = gamelift.createPlayerSession({
-                        GameSessionId: params.GameSessionId,
-                        PlayerId: uuid.v4()
-                    }).promise();
-                    // Because AWS
-                    let playerSession = (await playerSessionPromise).PlayerSession;
+        gameSession = await createGameSession(gameParams);
+        console.log(gameSession);
 
-                    response = {
-                        'statusCode': 200,
-                        'body': JSON.stringify({
-                            message: 'PlayerSession Creation Successful',
-                            PlayerSessionId: playerSession.PlayerSessionId,
-                            GameSessionId: playerSession.GameSessionId,
-                            FleetId: playerSession.FleetId,
-                            CreationTime: playerSession.CreationTime,
-                            Status: playerSession.Status,
-                            IpAddress: playerSession.IpAddress,
-                            Port: playerSession.Port.toString()
-                        })
-                    }
-                }
-                else {
-                    console.log("400 Bad Request - Body Missing");
-                    response = {
-                        'statusCode': 400,
-                        'body': JSON.stringify({
-                            message: 'Bad Request'
-                        })
-                    }
-                }
+        // Respond with HTTP 200 OK and Token
+        console.log('200 OK');
+        response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: 'Session Creation Successful',
+            GameSessionId: gameSession.GameSessionId,
+            FleetId: gameSession.FleetId,
+            CreationTime: gameSession.CreationTime,
+            CurrentPlayerSessionCount: gameSession.CurrentPlayerSessionCount,
+            MaximumPlayerSessionCount: gameSession.MaximumPlayerSessionCount,
+            Status: gameSession.Status.toString(),
+            IpAddress: gameSession.IpAddress.toString(),
+            Port: gameSession.Port.toString(),
+            PlayerSessionCreationPolicy: gameSession.PlayerSessionCreationPolicy,
+          }),
+        };
 
-                break;
+        break;
+      // Test Player Session POST - Create Player Session in Game Session
+      case '/test/player/session':
+        if (event.body) {
+          playerParams = JSON.parse(event.body);
+          console.log('Creating Player Session');
+          playerSession = await createPlayerSession(playerParams);
+
+          response = {
+            statusCode: 200,
+            body: JSON.stringify({
+              message: 'PlayerSession Creation Successful',
+              PlayerSessionId: playerSession.PlayerSessionId,
+              GameSessionId: playerSession.GameSessionId,
+              FleetId: playerSession.FleetId,
+              CreationTime: playerSession.CreationTime,
+              Status: playerSession.Status,
+              IpAddress: playerSession.IpAddress,
+              Port: playerSession.Port.toString(),
+            }),
+          };
+        } else {
+          console.log('400 Bad Request - Body Missing');
+          response = {
+            statusCode: 400,
+            body: JSON.stringify({
+              message: 'Bad Request',
+            }),
+          };
         }
-    } catch (err) {
-        console.log(err);
-        return err;
+
+        break;
+
+      default:
+        // Unknown Resource
+        console.log(`Unknown Resource: ${event.resource}`);
+        break;
     }
-    return response
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+  return response;
 };
